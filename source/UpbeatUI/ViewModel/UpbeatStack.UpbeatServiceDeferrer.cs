3ad8acf9bb93611e6950afdb3945eba49a2e41a9
michaelpduda <michael@pulselyre.com>
2024-01-03 20:47:17 -0800

    Deprecates ActionDeferrer

--- source/UpbeatUI/ViewModel/UpbeatStack.UpbeatServiceDeferrer.cs
+++ source/UpbeatUI/ViewModel/UpbeatStack.UpbeatServiceDeferrer.cs
@@ -3,16 +3,34 @@
  * https://github.com/pulselyre/upbeatui/blob/main/LICENSE.md
  */
 using System;
+using System.Collections.Generic;
 
 namespace UpbeatUI.ViewModel
 {
     public partial class UpbeatStack
     {
-        private class UpbeatServiceDeferrer : ActionDeferrer
+        private class UpbeatServiceDeferrer : IDisposable
         {
+            private readonly Queue<Action> _queue = new Queue<Action>();
+            private readonly Action _unlocker;
+
             public UpbeatServiceDeferrer(UpbeatService configurationService)
-                : base(configurationService.Lock, configurationService.Unlock)
-            { }
+            {
+                if (configurationService == null)
+                    throw new ArgumentNullException("unlocker action must be provided.");
+                _unlocker = configurationService.Unlock;
+                configurationService.Lock(Defer);
+            }
+
+            public void Dispose()
+            {
+                while (_queue.Count > 0)
+                    _queue.Dequeue()();
+                _unlocker();
+            }
+
+            private void Defer(Action action)
+                => _queue.Enqueue(action);
         }
     }
 }
